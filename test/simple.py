5124b1e428c384077d9c6675543d59c93a31a8ba
Michael Zoech <michi.zoech@gmail.com>
2018-12-03 20:27:34 -0800

    Fix tests

--- test/simple.py
+++ test/simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+import os
 import shlex
 import subprocess
 import tempfile
@@ -9,6 +10,7 @@ import unittest
 class Simple(unittest.TestCase):
 
     def setUp(self):
+        self.repodir = os.getcwd()
         self.tempdir = tempfile.TemporaryDirectory()
         self.temppath = Path(self.tempdir.name)
         self.run_hg('init')
@@ -20,23 +22,23 @@ class Simple(unittest.TestCase):
         subprocess.run(['hg'] + shlex.split(command), cwd=self.tempdir.name, check=True)
 
     def run_cin(self, command):
-        cmd = shlex.split(f"bash -c 'source /home/maik/projects/cinnabar/cinnabar.sh && COLOR=0 {command}'")
+        cmd = shlex.split(f"bash -c 'source {self.repodir}/cinnabar.sh && COLOR=0 {command}'")
         p=subprocess.run(cmd, cwd=self.tempdir.name, stdout=subprocess.PIPE)
         output = p.stdout.decode().strip()
         self.assertEqual(p.returncode, 0, output)
         return output
 
     def test_empty_repo_should_have_no_status(self):
-        output = self.run_cin('do_status')
+        output = self.run_cin('cin_do_status')
         self.assertEqual(output, '')
 
     def test_repo_with_one_file_should_show_file_as_untracked(self):
         (self.temppath / 'f0').touch()
-        output = self.run_cin('do_status')
+        output = self.run_cin('cin_do_status')
         self.assertEqual(output, '? [1] f0')
 
     def test_repo_with_added_file(self):
         (self.temppath / 'f0').touch()
         self.run_hg('add f0')
-        output = self.run_cin('do_status')
+        output = self.run_cin('cin_do_status')
         self.assertEqual(output, 'A [1] f0')
